5bc02a2e397b2f5868e73f5da0d120dd570b180e
Sanket Kanase <148350769+sanketkanse999@users.noreply.github.com>
2026-08-25 15:25:20 +0530

    Configure frontend API for production

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// http://localhost:8080/api/
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
+
 export const GenerateImageFromPrompt = async (data) =>
   await API.post("/generateImage/", data);
